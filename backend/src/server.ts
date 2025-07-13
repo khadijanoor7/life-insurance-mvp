141bd5326a323be0dae58fd8c05d094dffc4c27d
Khadija Noor <dev.khadijanoor@gmail.com>
2025-07-12 23:40:46 -0700

    API issue fix

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -18,6 +18,7 @@ import { userRoutes } from "./routes/user";
 dotenv.config();
 
 const app = express();
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 5001;
 
 // Security middleware
